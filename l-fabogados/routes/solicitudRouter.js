50928b04fa21c24b1d874210170361c5993c4e50
Gustavo Castillo <gustavoa.castillo@correo.usa.edu.co>
2022-03-10 09:35:58 -0500

    Se realizo El REST para /:solicitudid

--- l-fabogados/routes/solicitudRouter.js
+++ l-fabogados/routes/solicitudRouter.js
@@ -41,4 +41,39 @@ solicitudRouter.route('/')
     .catch((err)=>next(err))
 })
 
+//solicitud por id
+solicitudRouter.route('/:solicitudid')
+.get((req,res,next)=>{
+    Solicitud.findById(req.params.solicitudid)
+    .then((solicitud)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(solicitud);
+    },(err)=>next(err))
+    .catch((err)=>next(err))
+})
+.post((req,res,next)=>{
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /solicitudes');
+})
+.put((req,res,next)=>{
+    Solicitud.findOneAndUpdate(req.params.solicitudid,{$set:req.body},{new:true})
+    .then((solicitud)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(solicitud);
+    },(err)=>next(err))
+    .catch((err)=>next(err))
+})
+.delete((req,res,next)=>{
+    Solicitud.findByIdAndRemove(req.params.solicitudid)
+    .then((resp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    },(err)=>next(err))
+    .catch((err)=>next(err))
+})
+
+
 module.exports=solicitudRouter;
